0763155aaf24e98ee7f3e920c7d7ee2d88b1b913
gyorizsuzs <gyorizsuzs@gmail.com>
2023-08-09 11:57:54 +0200

    creating card w toggle fn betw line n bar charts

--- my-app/src/MyApp.jsx
+++ my-app/src/MyApp.jsx
@@ -1,5 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, CardHeader, Text } from "@ui5/webcomponents-react";
+import { spacing } from "@ui5/webcomponents-react-base";
+import { BarChart, LineChart } from "@ui5/webcomponents-react-charts";
+
+const dataset = [
+  {
+    month: "January",
+    data: 65,
+  },
+  {
+    month: "February",
+    data: 59,
+  },
+  {
+    month: "March",
+    data: 80,
+  },
+  {
+    month: "April",
+    data: 81,
+  },
+  {
+    month: "May",
+    data: 56,
+  },
+  {
+    month: "June",
+    data: 55,
+  },
+  {
+    month: "July",
+    data: 40,
+  },
+];
 
 export function MyApp() {
-  return <div>My root component</div>;
+  const [toggleCharts, setToggleCharts] = useState("lineChart");
+
+  const handleHeaderClick = () => {
+    if (toggleCharts === "lineChart") {
+      setToggleCharts("barChart");
+    } else {
+      setToggleCharts("lineChart");
+    }
+  };
+
+  return (
+    <div>
+      <Card
+        header={
+          <CardHeader
+            titleText="Card"
+            interactive
+            onClick={handleHeaderClick}
+          />
+        }
+        style={{ width: "300px" }}
+      >
+        <Text style={spacing.sapUiContentPadding}>
+          This is the content area of the Card
+        </Text>
+        {toggleCharts === "lineChart" ? (
+          <LineChart
+            dimensions={[{ accessor: "month" }]}
+            measures={[{ accessor: "data", label: "Stock Price" }]}
+            dataset={dataset}
+          />
+        ) : (
+          <BarChart
+            dimensions={[{ accessor: "month" }]}
+            measures={[{ accessor: "data" }]}
+            dataset={dataset}
+          />
+        )}
+      </Card>
+    </div>
+  );
 }
